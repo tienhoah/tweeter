978d3460a60e095d1b21c29512345ba6ae46b668
tienhoah <nickhoang11@gmail.com>
2018-08-15 23:27:44 +0000

    change hover method to (mouseenter and mouseleave) events.

--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,11 +1,12 @@
 $(function() {
   var tweetArticle = $("article.tweet");
-
-  tweetArticle.hover(function() {
+  console.log(tweetArticle);
+  $('body').on('mouseenter', 'article.tweet', function() {
     $(this).css("border", "1px solid #000");
     $(this).find("header").css('opacity', '1');
     $(this).children().find("i").css('visibility', 'visible');
-  }, function() {
+  });
+  $('body').on('mouseleave', 'article.tweet', function() {
     $(this).css("border", "1px solid #ccc");
     $(this).find("header").css('opacity', '0.5');
     $(this).children().find("i").css('visibility', 'hidden');
