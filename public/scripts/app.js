aae30a29c39af9257eb55b5dd5ffa557fe530b83
tienhoah <nickhoang11@gmail.com>
2018-08-15 21:18:28 +0000

    update Tweet article css rounded at bottom.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,23 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
 
 $(function() {
+
+  function renderTweets(tweets) {
+
+    for (var i = 0; i < tweets.length; i++){
+      var $tweetItem = createTweetElement(tweets[i]);
+      $('#tweet-area').append($tweetItem);
+    }
+  }
+
 
   function createTweetElement(tweetdb) {
     var miliseconds = tweetdb["created_at"];
@@ -28,7 +69,7 @@ $(function() {
     var diffTime = myCurDate - miliseconds;
     var totalDays = Math.floor(diffTime / 86400000);
 
-    var $article = $("<article>").addClass("tweet");
+    var $tweet = $("<article>").addClass("tweet");
     var $header = $("<header>");
     var $footer = $("<footer>");
     var $h1 = $("<h1>").addClass("username");
@@ -52,20 +93,18 @@ $(function() {
     $h1.appendTo($header);
     $img.appendTo($header);
     $p_header.appendTo($header);
-    $article.append($header);
+    $tweet.append($header);
 
-    $article.append($p_body);
+    $tweet.append($p_body);
 
     $span.appendTo($footer);
     $i_heart.appendTo($footer);
     $i_retweet.appendTo($footer);
     $i_flag.appendTo($footer);
-    $article.append($footer);
+    $tweet.append($footer);
 
-    return $article;
+    return $tweet;
   }
+  renderTweets(data);
 
-  var $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  console.log($('#tweet-area').append($tweet));
 });
