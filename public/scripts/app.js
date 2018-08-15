e9336977269cee6620af5d1d10174283ffaa1ee7
tienhoah <nickhoang11@gmail.com>
2018-08-15 21:00:24 +0000

    create hover effect, create Dynamic Tweet Box in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+$(function() {
+
+  function createTweetElement(tweetdb) {
+    var miliseconds = tweetdb["created_at"];
+    var myCurDate = new Date().getTime();
+    var diffTime = myCurDate - miliseconds;
+    var totalDays = Math.floor(diffTime / 86400000);
+
+    var $article = $("<article>").addClass("tweet");
+    var $header = $("<header>");
+    var $footer = $("<footer>");
+    var $h1 = $("<h1>").addClass("username");
+    var $p_body = $("<p>").addClass("comment");
+    var $img = $("<img>").addClass("userlogo");
+    var $p_header = $("<p>").addClass("usertag");
+    var $i_retweet = $("<i>").addClass("fa fa-retweet");
+    var $i_heart = $("<i>").addClass("fa fa-heart");
+    var $i_flag = $("<i>").addClass("fa fa-flag");
+    var $span = $("<span>").addClass("timestamp");
+
+    $h1.text(tweetdb['user']['name']);
+    $p_header.text(tweetdb['user']['handle']);
+    profile_pic = tweetdb['user']['avatars']['small'];
+    profile_pic_add = `src="${profile_pic}"`;
+    $img.attr({src: `${profile_pic}`});
+    $span.text(`${totalDays} days`);
+
+    $p_body.text(tweetdb['content']['text']);
+
+    $h1.appendTo($header);
+    $img.appendTo($header);
+    $p_header.appendTo($header);
+    $article.append($header);
+
+    $article.append($p_body);
+
+    $span.appendTo($footer);
+    $i_heart.appendTo($footer);
+    $i_retweet.appendTo($footer);
+    $i_flag.appendTo($footer);
+    $article.append($footer);
+
+    return $article;
+  }
+
+  var $tweet = createTweetElement(tweetData);
+  console.log($tweet);
+  console.log($('#tweet-area').append($tweet));
+});
--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,17 +1,13 @@
 $(function() {
   var tweetArticle = $("article.tweet");
-  console.log(tweetArticle.children().find("i"));
+
   tweetArticle.hover(function() {
-    $(this).parent().css('border', 'solid 1px');
-    $(this).children().find("h1.username").css('opacity', '1');
-    $(this).children().find("img.userlogo").css('opacity', '1');
-    $(this).children().find("p.usertag").css('opacity', '1');
-    $(this).children().find("i[name='icon']").css('visibility', 'visible');
+    $(this).css("border", "1px solid #000");
+    $(this).find("header").css('opacity', '1');
+    $(this).children().find("i").css('visibility', 'visible');
   }, function() {
-    $(this).parent().css('border', 'solid 1px #ccc');
-    $(this).children().find("h1.username").css('opacity', '0.5');
-    $(this).children().find("img.userlogo").css('opacity', '0.5');
-    $(this).children().find("p.usertag").css('opacity', '0.5');
-    $(this).children().find("i[name='icon']").css('visibility', 'hidden');
+    $(this).css("border", "1px solid #ccc");
+    $(this).find("header").css('opacity', '0.5');
+    $(this).children().find("i").css('visibility', 'hidden');
   });
 });
