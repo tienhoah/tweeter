f25b6468bb96c9844cc2f4f7649692c0abbb810d
tienhoah <nickhoang11@gmail.com>
2018-08-16 19:48:22 +0000

    add Compose button on the right of nav bar.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,34 @@
-function findSibling(child, sibling){
-  return $(child).parent().find(sibling);
-}
 
-function setCounter(element, value) {
-  $ (element).text(`${value}`);
-  if (value < 0) {
-    $ (value).css('color', 'red');
-  } else {
-    $ (value).css('color', 'black');
+$(function() {
+  function findSibling(child, sibling){
+    return $(child).parent().find(sibling);
   }
-}
-var updateCharCount = function(event) {
+
+  function setCounter(element, value) {
+    $ (element).text(`${value}`);
+    if (value < 0) {
+      $ (element).css('color', 'red');
+    } else {
+      $ (element).css('color', 'black');
+    }
+  }
+  var updateCharCount = function(event) {
     const maxChar = 140;
     const textLen = $(this).val().length;
     const diff = maxChar - textLen;
     const counter = findSibling(this, "span.counter");
     setCounter(counter, diff);
-}
+  }
 
-$(function() {
   const textArea = $("textarea[name=text]");
-  const tweetArticleHeader = $("article.tweet header");
-  textArea.on('keydown', updateCharCount);
-});
+  const composeBox = $("section.new-tweet");
+  const composeButton = $("button.compose")
 
+  textArea.on('keydown', updateCharCount);
+
+  composeButton.on("click", function(event) {
+    composeBox.slideToggle("slow", function() {
+      textArea.focus();
+    });
+  });
+});
