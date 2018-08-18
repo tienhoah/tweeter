9a80eb144b3f2c8a8b978ba1289753944cd8e291
tienhoah <nickhoang11@gmail.com>
2018-08-18 01:02:46 +0000

    sixth time on heroku fix

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const bodyParser    = require("body-parser");
 const app           = express();
 const {MongoClient} = require("mongodb");
 // const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
-// const MONGODB_URI = "mongodb://127.0.0.1:27017/tweets"
+// const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter"
 const MONGODB_URI = process.env.MONGODB_URI;
 
 
