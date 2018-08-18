06123d785a7283e567067424a922946f0f669907
tienhoah <nickhoang11@gmail.com>
2018-08-18 02:33:56 +0000

    fixing listening port

--- server/index.js
+++ server/index.js
@@ -32,9 +32,10 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
 
-  app.listen(PORT, () => {
-    console.log("Example app listening on port " + PORT);
-  });
+  app.listen(process.env.PORT || 5000);
+  // app.listen(PORT, () => {
+  //   console.log("Example app listening on port " + PORT);
+  // });
 });
 
 
